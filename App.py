a9adb1727c14dd109b3c6946b94f2ba0ad3a0b57
Damrong Tongsiri <damrong_golf@hotmail.com>
2021-07-31 19:16:02 +0700

    added virtualcam loop thread on App.py

--- App.py
+++ App.py
@@ -1,20 +1,32 @@
+import threading
 from tkinter import *
 from tkinter import colorchooser
+import tkinter
 from tkinter.ttk import *
 from tkinter.filedialog import askopenfile
-
+from service import Service
 
 class App(Frame):
-    def __init__(self, root):
+    def __init__(self, root:Tk):
         self.root = root
         root.title("PROJECT_NAME_HERE")
-
+        # widget preparation
         self._threshold_value = DoubleVar()
         self._mode_status = StringVar(value='normal')
         self._secondary_mode_status = StringVar(value='img')
         self.__createThresholdWidget()
         self.__createModeWidget()
         self.__createSecondaryModeWidget(self._mode_status.get())
+        # videostream service
+        self.stopEvent = threading.Event()
+        self.service = Service(self.stopEvent)
+        self.thread = threading.Thread(target = self.service.run_service_loop,args=())
+        self.thread.start()
+        self.root.wm_protocol("WM_DELETE_WINDOW",self.__onClose)
+
+    def __onClose(self):
+        self.stopEvent.set()
+        self.root.quit()
 
     def __createThresholdWidget(self):
         self.threshold_label = Label(root, text='Threshold')
@@ -48,16 +60,16 @@ class App(Frame):
             print(self._mode_status.get())
             self.__updateSecondaryModeWidget(self._mode_status.get())
 
-    # TODO: create event based image upload or color changing frame using given radio choice
     def __createSecondaryModeWidget(self, mode_str):
         self.secondary_mode_label = Label(root, text='Choose Background Color')
         self.secondary_mode_label.grid(row=3, column=1, sticky=W)
 
-        self.secondary_mode_option = Button(root, text="Choose Image", command=lambda: _choose_color())
+        self.secondary_mode_option = Button(
+            root, text="Choose Image", command=lambda: _choose_color())
         self.secondary_mode_option.grid(row=4, column=1, sticky=W)
 
         def _choose_color():
-            color_code = colorchooser.askcolor(title ="Choose color")
+            color_code = colorchooser.askcolor(title="Choose color")
             print(color_code)
 
     def __updateSecondaryModeWidget(self, mode_str):
@@ -66,18 +78,20 @@ class App(Frame):
         if mode_str == 'normal':
             self.secondary_mode_label = Label(
                 root, text='Choose Background Color')
-            self.secondary_mode_option = Button(root, text="Select color", command=lambda: _choose_color())
+            self.secondary_mode_option = Button(
+                root, text="Select color", command=lambda: _choose_color())
 
         elif mode_str == 'image':
             self.secondary_mode_label = Label(
                 root, text='Choose Background Image')
-            self.secondary_mode_option = Button(root, text="Choose Image", command=lambda: _choose_color())
+            self.secondary_mode_option = Button(
+                root, text="Choose Image", command=lambda: _choose_color())
 
         self.secondary_mode_label.grid(row=3, column=1, sticky=W)
         self.secondary_mode_option.grid(row=4, column=1, sticky=W)
-        
+
         def _choose_color():
-            color_code = colorchooser.askcolor(title ="Choose color")
+            color_code = colorchooser.askcolor(title="Choose color")
             print(color_code)
 
 
---
+++ service.py
@@ -0,0 +1,39 @@
+import cv2
+import threading
+from tkinter import *
+from utils import segment
+from pyvirtualcam import PixelFormat, Camera
+
+# TODO: GUI
+# TODO: some video capture selection
+# TODO: threshold value slider
+# TODO: image path browser for replacement
+# TODO: mode swapping (gaussian blur ?)
+# TODO: make it's executionable in both windows and macOS (or just windows)
+
+# Query final capture device values
+# (may be different from preferred settings)
+class Service():
+    def __init__(self, stopEvent:threading.Event):
+        self.cap = cv2.VideoCapture(1)
+        self.width = int(self.cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+        self.height = int(self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        self.fps = self.cap.get(cv2.CAP_PROP_FPS)
+        self.default_image_path = './test_img/someone.jpg'
+        self.segment_utils = segment.SegmentUtils()
+        self.stopEvent = stopEvent
+
+    def run_service_loop(self):
+        with Camera(self.width, self.height, self.fps, fmt=PixelFormat.BGR) as cam:
+            print('Virtual camera device: ' + cam.device)
+            self.segment_utils.set_default_img(self.default_image_path, (self.width, self.height))
+            while self.stopEvent.is_set():
+                success, image = self.cap.read()
+                image = cv2.flip(image, 1)
+                image = self.segment_utils.away_from_screen_correction(image)
+                output_image = self.segment_utils.segment_human_out(image)
+                cam.send(output_image)
+                cam.sleep_until_next_frame()
+        self.cap.release()
+
+
