28d6f92649db78bd0a851c06ab392e31c2084fc2
Damrong Tongsiri <damrong_golf@hotmail.com>
2021-07-31 18:35:17 +0700

    added secondary widget

--- App.py
+++ App.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import colorchooser
 from tkinter.ttk import *
 from tkinter.filedialog import askopenfile
 
@@ -13,11 +14,11 @@ class App(Frame):
         self._secondary_mode_status = StringVar(value='img')
         self.__createThresholdWidget()
         self.__createModeWidget()
-        
+        self.__createSecondaryModeWidget(self._mode_status.get())
 
     def __createThresholdWidget(self):
-        self.threshold_label = Label(root, text='Threshold').grid(
-            row=0, column=0, sticky=W)
+        self.threshold_label = Label(root, text='Threshold')
+        self.threshold_label.grid(row=0, column=0, sticky=W)
 
         self.threshold = Scale(root, variable=self._threshold_value, from_=0, to=1,
                                orient=HORIZONTAL, command=lambda *a: _slider_select(*a))
@@ -31,7 +32,8 @@ class App(Frame):
         # update select radio button (nornal/ blur)
         # Normal/Blur/Image
         self.mode_label = Label(
-            root, text='Normal/Blur/Image:').grid(row=1, column=0, sticky=W)
+            root, text='Normal/Blur/Image:')
+        self.mode_label.grid(row=1, column=0, sticky=W)
         self.radio_normal = Radiobutton(
             root, text='Normal', variable=self._mode_status, value='normal', command=lambda: _radio_mode_select())
         self.radio_normal.grid(row=2, column=0, sticky=W)
@@ -44,14 +46,42 @@ class App(Frame):
 
         def _radio_mode_select(*args):
             print(self._mode_status.get())
-            self.__createSecondaryModeWidget(self._mode_status.get())
+            self.__updateSecondaryModeWidget(self._mode_status.get())
 
-    #TODO: create event based image upload or color changing frame using given radio choice
-    def __createSecondaryModeWidget(self,mode_str):
-        pass
+    # TODO: create event based image upload or color changing frame using given radio choice
+    def __createSecondaryModeWidget(self, mode_str):
+        self.secondary_mode_label = Label(root, text='Choose Background Color')
+        self.secondary_mode_label.grid(row=3, column=1, sticky=W)
+
+        self.secondary_mode_option = Button(root, text="Choose Image", command=lambda: _choose_color())
+        self.secondary_mode_option.grid(row=4, column=1, sticky=W)
+
+        def _choose_color():
+            color_code = colorchooser.askcolor(title ="Choose color")
+            print(color_code)
+
+    def __updateSecondaryModeWidget(self, mode_str):
+        self.secondary_mode_label.destroy()
+        self.secondary_mode_option.destroy()
+        if mode_str == 'normal':
+            self.secondary_mode_label = Label(
+                root, text='Choose Background Color')
+            self.secondary_mode_option = Button(root, text="Select color", command=lambda: _choose_color())
+
+        elif mode_str == 'image':
+            self.secondary_mode_label = Label(
+                root, text='Choose Background Image')
+            self.secondary_mode_option = Button(root, text="Choose Image", command=lambda: _choose_color())
+
+        self.secondary_mode_label.grid(row=3, column=1, sticky=W)
+        self.secondary_mode_option.grid(row=4, column=1, sticky=W)
+        
+        def _choose_color():
+            color_code = colorchooser.askcolor(title ="Choose color")
+            print(color_code)
 
 
 root = Tk()
-root.geometry('360x200')
+root.geometry('360x360')
 my_gui = App(root)
 root.mainloop()
