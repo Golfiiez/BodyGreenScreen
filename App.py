0407c83d0728a89c57515ce53d401fc495e1e9c3
Damrong Tongsiri <damrong_golf@hotmail.com>
2021-07-31 21:47:05 +0700

    change function casing

--- App.py
+++ App.py
@@ -26,9 +26,9 @@ class App(Frame):
         self.thread = threading.Thread(
             target=self.service.run_service_loop, args=())
         self.root.wm_protocol("WM_DELETE_WINDOW", self.__onClose)
-        self.__initvcamservice()
+        self.__initVCamService()
 
-    def __initvcamservice(self):
+    def __initVCamService(self):
         try:
             self.thread.start()
         except Exception as e:
@@ -45,11 +45,11 @@ class App(Frame):
         self.threshold_label.grid(row=0, column=0, sticky=W)
 
         self.threshold = Scale(root, variable=self._threshold_value, from_=0, to=1,
-                               orient=HORIZONTAL, command=lambda *a: _slider_select(*a))
+                               orient=HORIZONTAL, command=lambda *a: _sliderSelect(*a))
         self.threshold.grid(row=0, column=1, sticky=W)
         # update treshold
 
-        def _slider_select(*args):
+        def _sliderSelect(*args):
             threshold_value = self._threshold_value.get()
             self.service.segment_utils.update_threshold(threshold_value)
 
@@ -60,16 +60,16 @@ class App(Frame):
             root, text='Normal/Blur/Image:')
         self.mode_label.grid(row=1, column=0, sticky=W)
         self.radio_normal = Radiobutton(
-            root, text='Normal', variable=self._mode_status, value='normal', command=lambda: _radio_mode_select())
+            root, text='Normal', variable=self._mode_status, value='normal', command=lambda: _radioModeSelect())
         self.radio_normal.grid(row=2, column=0, sticky=W)
         self.radio_blur = Radiobutton(root, text='Gaussian blur', variable=self._mode_status,
-                                      value='blur', command=lambda: _radio_mode_select())
+                                      value='blur', command=lambda: _radioModeSelect())
         self.radio_blur.grid(row=2, column=1, sticky=W)
         self.radio_image = Radiobutton(root, text='Custom Image', variable=self._mode_status,
-                                       value='image', command=lambda: _radio_mode_select())
+                                       value='image', command=lambda: _radioModeSelect())
         self.radio_image.grid(row=2, column=2, sticky=W)
 
-        def _radio_mode_select(*args):
+        def _radioModeSelect(*args):
             print(self._mode_status.get())
             self.__updateSecondaryModeWidget(self._mode_status.get())
 
@@ -78,10 +78,10 @@ class App(Frame):
         self.secondary_mode_label.grid(row=3, column=1, sticky=W)
 
         self.secondary_mode_option = Button(
-            root, text="Select Color", command=lambda: _choose_color())
+            root, text="Select Color", command=lambda: _chooseColor())
         self.secondary_mode_option.grid(row=4, column=1, sticky=W)
 
-        def _choose_color():
+        def _chooseColor():
             color_code = colorchooser.askcolor(title="Choose color")
             print(color_code)
 
@@ -92,21 +92,32 @@ class App(Frame):
             self.secondary_mode_label = Label(
                 root, text='Choose Background Color')
             self.secondary_mode_option = Button(
-                root, text="Select Color", command=lambda: _choose_color())
+                root, text="Select Color", command=lambda: _chooseColor())
 
         elif mode_str == 'image':
             self.secondary_mode_label = Label(
                 root, text='Choose Background Image')
             self.secondary_mode_option = Button(
-                root, text="Choose Image", command=lambda: _choose_color())
+                root, text="Choose Image", command=lambda: _chooseColor())
 
         self.secondary_mode_label.grid(row=3, column=1, sticky=W)
         self.secondary_mode_option.grid(row=4, column=1, sticky=W)
 
-        def _choose_color():
+        def _chooseColor():
             color_code = colorchooser.askcolor(title="Choose color")
             print(color_code)
 
+    def __createAwayFromScreenWidget(self, mode_str):
+        self.secondary_mode_label = Label(root, text='Choose Background Color')
+        self.secondary_mode_label.grid(row=3, column=1, sticky=W)
+
+        self.secondary_mode_option = Button(
+            root, text="Select Color", command=lambda: _chooseColor())
+        self.secondary_mode_option.grid(row=4, column=1, sticky=W)
+
+        def _chooseColor():
+            color_code = colorchooser.askcolor(title="Choose color")
+            print(color_code)
 
 root = Tk()
 root.geometry('360x360')
