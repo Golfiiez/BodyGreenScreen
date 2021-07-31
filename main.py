e4503b387b66e101fd8c6762a06ddbeb295b4a7a
Damrong Tongsiri <damrong_golf@hotmail.com>
2021-07-31 15:52:03 +0700

    refactored default_img to show you image (w/o) greenscreen when you are not present in camera and redesigned SegmentUtils constructor

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from utils import segment
 from pyvirtualcam import PixelFormat, Camera
 
 # TODO: GUI
-# TODO: some video capture selection 
+# TODO: some video capture selection
 # TODO: threshold value slider
 # TODO: image path browser for replacement
 # TODO: mode swapping (gaussian blur ?)
@@ -15,18 +15,18 @@ cap = cv2.VideoCapture(1)
 width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 fps = cap.get(cv2.CAP_PROP_FPS)
-default_image_path = './temp/Screen Shot 2564-07-31 at 14.19.00.png'
+default_image_path = './test_img/someone.jpg'
 
 segment_utils = segment.SegmentUtils()
 
 with Camera(width, height, fps, fmt=PixelFormat.BGR) as cam:
     print('Virtual camera device: ' + cam.device)
-    segment_utils.set_default_img(  default_image_path, ( width, height ) )
+    segment_utils.set_default_img(default_image_path, (width, height))
     while cap.isOpened():
         success, image = cap.read()
         image = cv2.flip(image, 1)
-        output_image = segment_utils.segment_human_out( image )
-        cam.send(output_image)
+        output_image = segment_utils.segment_human_out(image)
+        cam.send(image)
         cam.sleep_until_next_frame()
-        
+
 cap.release()
--- utils/segment.py
+++ utils/segment.py
@@ -1,14 +1,16 @@
-import cv2 
+import cv2
 # unused import but will do it for gaussian blur and other choices
 import mediapipe as mp
 import numpy as np
 mp_drawing = mp.solutions.drawing_utils
 mp_selfie_segmentation = mp.solutions.selfie_segmentation
+
+
 class SegmentUtils():
-    def __init__(self) -> None:
+    def __init__(self, threshold=0.7, bg_color=(100, 255, 100)) -> None:
         self.selfie_segmentation = mp_selfie_segmentation.SelfieSegmentation(
             model_selection=0)
-        self._MASK_COLOR = (100, 255, 100),
+        self._MASK_COLOR = bg_color
         self._BG_IMAGE = None
         self._DEF_IMAGE = None
         self._DEF_COND = None
@@ -16,36 +18,37 @@ class SegmentUtils():
         # set mode
         self._MODE_DEF_IMG = False
         self._MODE_BG_IMG = False
-        self._MODE_BG_IMG = False
 
-    def set_bg_color(self, bg_color:tuple):
+        # set default threshold
+        self._THRESHOLD = threshold
+
+    def set_bg_color(self, bg_color: tuple):
         self._MASK_COLOR = bg_color
-    
+
     def set_bg_image(self, path_to_image):
         self._BG_IMAGE = cv2.imread(path_to_image)
 
-    def set_default_img( self, path_to_uploaded_img : str, frame_dim : tuple, threshold = 0.7 ):
+    def set_default_img(self, path_to_uploaded_img: str, frame_dim: tuple):
         image = cv2.imread(path_to_uploaded_img)
-        image = cv2.resize( image, frame_dim, interpolation = cv2.INTER_AREA )
+        image = cv2.resize(image, frame_dim, interpolation=cv2.INTER_AREA)
         results = self.selfie_segmentation.process(image)
-        condition = np.stack((results.segmentation_mask,) * 3, axis=-1) >= threshold
-        masked_image = np.zeros(image.shape, dtype=np.uint8)
-        masked_image[:] = self._MASK_COLOR
+        condition = np.stack((results.segmentation_mask,)
+                             * 3, axis=-1) >= self._THRESHOLD
 
         self._DEF_COND = condition
-        self._DEF_IMAGE = masked_image
+        self._DEF_IMAGE = image
 
     def is_human_present(self, condition):
         return np.sum(np.invert(condition)) > 0
 
-    def display_default_img(self, frame, threshold=0.7):
-        output_image = np.where( self._DEF_COND, self._DEF_IMAGE, frame ) 
+    def display_default_img(self, frame):
+        output_image = np.where(self._DEF_COND, self._DEF_IMAGE, frame)
         return output_image
-        
-    def segment_human_out(self, image, threshold = 0.7):
+
+    def segment_human_out(self, image):
         results = self.selfie_segmentation.process(image)
         condition = np.stack(
-            (results.segmentation_mask,) * 3, axis=-1) <= threshold
+            (results.segmentation_mask,) * 3, axis=-1) <= self._THRESHOLD
         # The background can be customized.
         #   a) Load an image (with the same width and height of the input image) to
         #      be the background, e.g., bg_image = cv2.imread('/path/to/image/file')
@@ -60,4 +63,4 @@ class SegmentUtils():
         output_image = np.where(condition, image, masked_image)
 
         return output_image
-    
+
